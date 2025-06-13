acc7a949a2cf68e7cf8f0e9b984b198f8895d2cd
W-Kuah <5501552+W-Kuah@users.noreply.github.com>
2025-06-13 22:09:12 +1000

    llm md

--- src/components/LlmRecipe.jsx
+++ src/components/LlmRecipe.jsx
@@ -1,8 +1,11 @@
+import ReactMarkdown from "react-markdown";
+
 export default function LlmRecipe(props) {
     const {recipe} = props;
     return (
-        <>
-            {recipe}
-        </>
+        <section className="suggested-recipe-section">
+            <h2>MindMeal Suggests:</h2>
+            <ReactMarkdown>{recipe}</ReactMarkdown>
+        </section>
     );
 }
